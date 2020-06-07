d40da8d0c0c4219f5eba2cfeac1c08b28f6bb783
Rath-san <rim.kozlowski@gmail.com>
2020-06-07 20:05:18 +0200

    First commit

---
+++ component---src-pages-404-tsx-ef991d448f6976c45b77.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t("soUV");t("PGGC");e.default=function(n){return o.createElement(u.a,null)}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ef991d448f6976c45b77.js.map
